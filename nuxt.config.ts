0681c228f2d776ddc1b4d67717698a31edc285ca
razmatinyan <razmatinyan28@gmail.com>
2026-09-03 12:16:40 +0400

    init: set-up the environment before project start

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	vite: {
+		plugins: [tailwindcss()],
+		// optimizeDeps: {
+		// 	include: [
+		// 		'@vue/devtools-core',
+		// 		'@vue/devtools-kit',
+		// 		'@vueuse/core',
+		// 		'reka-ui',
+		// 		'zod',
+		// 		'clsx',
+		// 		'tailwind-merge',
+		// 		'gsap',
+		// 		'gsap/ScrollTrigger',
+		// 		'gsap/SplitText',
+		// 		'gsap/all',
+		// 		'@lucide/vue',
+		// 		'class-variance-authority',
+		// 	],
+		// },
+	},
+})
