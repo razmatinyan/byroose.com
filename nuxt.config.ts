610eda27eb7f96a6f9bbc9fa59bbae71802e8277
razmatinyan <razmatinyan28@gmail.com>
2026-09-03 12:21:11 +0400

    packages: add nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,26 +2,29 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
-	devtools: { enabled: true },
-	vite: {
-		plugins: [tailwindcss()],
-		// optimizeDeps: {
-		// 	include: [
-		// 		'@vue/devtools-core',
-		// 		'@vue/devtools-kit',
-		// 		'@vueuse/core',
-		// 		'reka-ui',
-		// 		'zod',
-		// 		'clsx',
-		// 		'tailwind-merge',
-		// 		'gsap',
-		// 		'gsap/ScrollTrigger',
-		// 		'gsap/SplitText',
-		// 		'gsap/all',
-		// 		'@lucide/vue',
-		// 		'class-variance-authority',
-		// 	],
-		// },
-	},
+ compatibilityDate: '2025-07-15',
+ devtools: { enabled: true },
+
+ vite: {
+																																																																																plugins: [tailwindcss()],
+																																																																																// optimizeDeps: {
+																																																																																// 	include: [
+																																																																																// 		'@vue/devtools-core',
+																																																																																// 		'@vue/devtools-kit',
+																																																																																// 		'@vueuse/core',
+																																																																																// 		'reka-ui',
+																																																																																// 		'zod',
+																																																																																// 		'clsx',
+																																																																																// 		'tailwind-merge',
+																																																																																// 		'gsap',
+																																																																																// 		'gsap/ScrollTrigger',
+																																																																																// 		'gsap/SplitText',
+																																																																																// 		'gsap/all',
+																																																																																// 		'@lucide/vue',
+																																																																																// 		'class-variance-authority',
+																																																																																// 	],
+																																																																																// },
+																},
+
+ modules: ['@nuxt/fonts', '@nuxt/icon', '@vueuse/nuxt'],
 })
