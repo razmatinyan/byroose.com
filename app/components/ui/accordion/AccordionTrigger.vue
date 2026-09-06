05f65902bfdb20e4ce8939e92ac6e3fbc2a11db4
razmatinyan <razmatinyan28@gmail.com>
2026-09-06 16:54:39 +0400

    fix(ui): overrotating of accordion icon on open

--- app/components/ui/accordion/AccordionTrigger.vue
+++ app/components/ui/accordion/AccordionTrigger.vue
@@ -20,7 +20,7 @@ const delegatedProps = reactiveOmit(props, 'class')
 			v-bind="delegatedProps"
 			:class="
 				cn(
-					'focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-xl font-medium transition-all outline-none focus-visible:ring-3 disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]_[data-slot=accordion-icon]]:rotate-180',
+					'focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-xl font-medium transition-all outline-none focus-visible:ring-3 disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]_[data-slot=accordion-icon]]:rotate-45',
 					props.class,
 				)
 			"
@@ -29,7 +29,7 @@ const delegatedProps = reactiveOmit(props, 'class')
 			<slot name="icon">
 				<span
 					data-slot="accordion-icon"
-					class="pointer-events-none grid size-9 shrink-0 place-items-center rounded-lg bg-primary text-primary-foreground transition-transform duration-200"
+					class="pointer-events-none grid size-9 shrink-0 place-items-center rounded-full bg-primary text-primary-foreground transition-transform duration-200"
 				>
 					<Icon :name="appIcons.plus" class="size-4" />
 				</span>
